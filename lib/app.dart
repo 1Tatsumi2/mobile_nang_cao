4f75d1c8e86158322d5679ed2d7b19d6ab7c1d51
hoanghuy <dinhhoanghuy20044@gmail.com>
2025-06-10 13:17:09 +0700

    event

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
